529359202c898c15b023b37c80b18cafdcf96ec8
Almanassik-Alarabi <almanassik.alarabi@gmail.com>
2025-08-06 17:43:10 +0100

    Update authGuard.js

--- agencie/js/authGuard.js
+++ agencie/js/authGuard.js
@@ -4,6 +4,6 @@
     // اسم التوكن حسب ما تستخدمه في مشروعك، عدله إذا كان مختلفاً
     const token = localStorage.getItem('agency_token');
     if (!token) {
-        window.location.href = '/public/agencie/login_agencie.html';
+        window.location.href = '/agencie/login_agencie.html';
     }
 })();
